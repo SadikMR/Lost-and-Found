aea8b457c3ab437ddb9370ad2bde53124f77a943
Md. Sadik Mahmud Raihan <sadik@Mds-MacBook-Air-2.local>
2025-01-07 00:41:48 +0600

    fixed errors for deploying client in render

--- Lost-and-found-client/vite.config.js
+++ Lost-and-found-client/vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: "dist", // Output directory for production build (default is 'dist')
+  },
+  base: "/", // Set the base path for the app (you can change this if it's hosted at a subdirectory)
+});
