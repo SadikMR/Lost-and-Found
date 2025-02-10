dc1393f216c61384bc72527cb31a1b50ce40b3fd
Md. Sadik Mahmud Raihan <sadikmahmudraihan@gmail.com>
2025-02-10 11:11:06 +0600

    added ownership verification system for found posts

--- Lost-and-found-server/index.js
+++ Lost-and-found-server/index.js
@@ -9,6 +9,7 @@ const foundPostRoutes = require("./routes/foundPostRoutes");
 const chatRoutes = require("./routes/chatRoutes");
 const errorHandler = require("./middleware/errorHandler");
 const userRoutes = require("./routes/userRoutes");
+const verificationRoutes = require("./routes/verificationRoutes");
 
 dotenv.config();
 
@@ -50,6 +51,7 @@ app.use("/posts/lost", lostPostRoutes);
 app.use("/posts/found", foundPostRoutes);
 app.use("/chat", chatRoutes);
 app.use("/user", userRoutes);
+app.use("/Verify", verificationRoutes);
 
 // Health Check
 app.get("/", (req, res) => {
